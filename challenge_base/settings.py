cc9317ecaa5dc0a0ad53f2caf598669595cea835
Delroy Brown <delroybrown.db@gmail.com>
2023-02-19 08:30:06 +0000

    Added dj-databse

--- challenge_base/settings.py
+++ challenge_base/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -56,14 +57,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'challenge_base.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
